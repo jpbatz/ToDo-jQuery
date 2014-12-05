6c474c04ec5865bede9d2f32ef23ade480cadb2a
jpbatz <jph@hawaii.edu>
2014-12-04 20:48:14 -1000

    MBA clears list and adds an item to the list

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,1 +1,18 @@
-alert("Hello!");
+$(function() {
+// $(document).ready(function() {  // when script.js is in .html head
+
+  // alert("Hello!");
+
+    $('#new-todo').keydown(function(e) {
+      // alert( "Handler for .keydown() called." );
+
+      if(e.keyCode == 13) {
+        // console.log("key pressed: " + e.keyCode);
+        // console.log($(this).val());
+        $('#list').append("<li>" + $(this).val() + "</li>")
+      };
+
+
+  });
+
+});
