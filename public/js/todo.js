02d99f2d61748202649b4504b2c2c132ceb41e7b
jpbatz <jph@hawaii.edu>
2014-12-04 21:02:53 -1000

    MBA gave class list-item to each li in the list

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,13 +3,16 @@ $(function() {
 
   // alert("Hello!");
 
+    // clear list
+    $('#list li').remove();
+
     $('#new-todo').keydown(function(e) {
       // alert( "Handler for .keydown() called." );
 
       if(e.keyCode == 13) {
         // console.log("key pressed: " + e.keyCode);
         // console.log($(this).val());
-        $('#list').append("<li>" + $(this).val() + "</li>")
+        $('#list').append('<li class="list-item">' + $(this).val() + '</li>')
       };
 
 
