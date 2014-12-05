15a8ba8058ce9cfb7246bd335b3e34b304524747
jpbatz <jph@hawaii.edu>
2014-12-04 21:56:08 -1000

    MBA Level 2 checkbox checked = line-through and unchecked = none

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,7 +3,7 @@ $(function() {
 
   // alert("Hello!");
 
-    // clear list
+    // clear list - uncomment after testing
     $('#list li').remove();
 
     $('#new-todo').keydown(function(e) {
@@ -12,10 +12,23 @@ $(function() {
       if(e.keyCode == 13) {
         // console.log("key pressed: " + e.keyCode);
         // console.log($(this).val());
-        $('#list').append('<li class="list-item">' + $(this).val() + '</li>')
+        $('#list').append('<li class="list-item"><input type="checkbox" class="item-checkbox" value="">' + $(this).val() + '</li>')
       };
-
-
   });
+
+
+  $('ul').on('change', '.item-checkbox', function() {
+    // console.log(this);
+    // console.log($(this));
+
+    if(this.checked) {
+      console.log('item was selected');
+      $(this).parent().css('text-decoration','line-through');
+    } else {
+      console.log('item was not selected');
+      $(this).parent().css('text-decoration','none');
+    }
+
+});    
 
 });
