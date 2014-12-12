65e253fa3b97ab3cbf77b5f87b5c6f4cdc6d55c0
jpbatz <jph@hawaii.edu>
2014-12-11 20:02:36 -1000

    MBA Uses the body-parsing middleware to parse the post request, and populate req.body

--- server/app.js
+++ server/app.js
@@ -1,27 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
+
+// middleware
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// route
 app.post('/save', function(req, res) {
-
-  console.log('user sent post request');  // see in cli resp to browser save
-  res.send("ok");
-
-  // fs.writeFile("list.json");
-  fs = require('fs');
-  fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
-    if (err) return console.log(err);
-      console.log('Hello World > helloworld.txt');
-  });
-
+  console.log('user sent post request');
+  console.log( req.body ); // prints body of bttp request
+  // res.send("success");  // see in IE Network-Preview
+  res.json({status:"success!!"}); // express will stringify
 });
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = server.address().address
+  var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port)
 
-});
+})
