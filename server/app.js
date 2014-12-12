c01ecc142fe7e89948a315d014441b7807396e04
jpbatz <jph@hawaii.edu>
2014-12-11 21:38:57 -1000

    MBA Level 6 complete, loads saved todo list from flat file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 var app = express();
 
 
@@ -9,10 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // route
 app.post('/save', function(req, res) {
-  console.log('user sent post request');
-  console.log( req.body ); // prints body of bttp request
-  // res.send("success");  // see in IE Network-Preview
-  res.json({status:"success!!"}); // express will stringify
+  // console.log('user sent post request');
+  // console.log( req.body ); // prints body of bttp request
+  // // res.send("success");  // see in IE Network-Preview
+  // res.json({status:"success!!"}); // express will stringify
+  saveToDoList(req.body.todo_json_data);
+  res.send("puppies!!")
 });
 
 var server = app.listen(3000, function () {
@@ -23,3 +26,12 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 })
+
+function saveToDoList(content) {
+
+  fs.writeFile('./public/todo_save.txt', content, function (err) {
+    if (err) return console.log(err);
+      console.log('Successfully saved todo_save.txt');
+  });
+
+}
